00387617036e79b04ec20ae84144a448e1081ab0
Mikhail <fentoru@gmail.com>
2022-11-22 11:25:08 +0500

    v1.98 added swiper animation

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	
+	public Vector3 launchVelocity;
+	public bool inPlay = false;
+	
+	private Rigidbody rigidBody;
+	private AudioSource audioSource;
+	
+	
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		rigidBody.useGravity = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Launch (Vector3 velocity)
+	{	
+		inPlay = true;
+		rigidBody.useGravity = true;
+		rigidBody.velocity = velocity;
+		
+		AudioSource audioSource = GetComponent<AudioSource>();
+		audioSource.Play ();
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/DragLaunch.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Ball))]
+public class DragLaunch : MonoBehaviour {
+
+	private Vector3 dragStart;
+	private Vector3 dragEnd;
+	private float startTime;
+	private float endTime;
+	private Ball ball;
+	private bool isLaunched;
+	
+	// Use this for initialization
+	void Start () {
+		ball = GetComponent<Ball>();
+		isLaunched = false;
+	}
+
+	public void DragStart(){
+		dragStart = Input.mousePosition;
+		startTime = Time.time;
+	}
+	
+	public void DragEnd(){
+		if (!ball.inPlay){
+			dragEnd = Input.mousePosition;
+			endTime = Time.time;
+			
+			float dragDuration = endTime - startTime;
+			
+			float launchSpeedX = (dragEnd.x - dragStart.x) / dragDuration;
+			float launchSpeedZ = (dragEnd.y - dragStart.y) / dragDuration;
+			
+			Vector3 launchVelocity = new Vector3(launchSpeedX, 0, launchSpeedZ);
+			ball.Launch(launchVelocity);
+			isLaunched = true;
+		}
+	}
+	
+	public void MoveStart(float amount){
+		if(!ball.inPlay){
+			ball.transform.Translate(new Vector3(amount, 0, 0));
+		}
+	}
+}
+
+
+
+
+
+
