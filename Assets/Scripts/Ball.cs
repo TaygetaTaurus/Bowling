25b080528454ab01e51bfd3c3949d6d173868a85
Mikhail <fentoru@gmail.com>
2022-11-22 15:00:21 +0500

    v2.02 added delay for score and ball returning

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -9,17 +9,21 @@ public class Ball : MonoBehaviour {
 	
 	private Rigidbody rigidBody;
 	private AudioSource audioSource;
+	private Vector3 ballStartPos;
 	
 	
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody>();
 		rigidBody.useGravity = false;
+		
+		ballStartPos = transform.position;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
 	public void Launch (Vector3 velocity)
@@ -30,8 +34,14 @@ public class Ball : MonoBehaviour {
 		
 		AudioSource audioSource = GetComponent<AudioSource>();
 		audioSource.Play ();
-		
-		
+	}
+	
+	public void Reset(){
+		inPlay = false;
+		transform.position = ballStartPos;
+		rigidBody.useGravity = false;
+		rigidBody.velocity = new Vector3 (0f, 0f, 0f);
+		rigidBody.angularVelocity = new Vector3 (0f, 0f, 0f);
 	}
 }
 
--- Assets/Scripts/PinSetter.cs
+++ Assets/Scripts/PinSetter.cs
@@ -4,20 +4,49 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PinSetter : MonoBehaviour {
-
+	public int lastStandingCount = -1;
 	public Text standingDisplay;
 	
+	private Ball ball;
+	private float lastChangeTime;
 	private bool ballEnteredBox = false;
 	
 	// Use this for initialization
 	void Start () {
-		
+		ball = GameObject.FindObjectOfType<Ball>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		standingDisplay.text = CountStanding().ToString();
+		
+		if (ballEnteredBox){
+			CheckStanding();
+		}
 	}
+	
+	void CheckStanding(){
+		int currentStanding = CountStanding();
+		
+		if (currentStanding != lastStandingCount){
+			lastChangeTime = Time.time;
+			lastStandingCount = currentStanding;
+			return;
+		}
+		
+		float settleTime = 3f;
+		if((Time.time - lastChangeTime) > settleTime){
+			PinsHaveSettled();
+		}
+	}
+	
+	void PinsHaveSettled(){
+		ball.Reset();
+		lastStandingCount = -1;
+		ballEnteredBox = false;
+		standingDisplay.color = Color.green;
+	}
+	
 	
 	int CountStanding(){
 		int standing = 0;
@@ -39,12 +68,7 @@ public class PinSetter : MonoBehaviour {
 	
 	void OnTriggerExit(Collider collider){
 		GameObject thingLeft = collider.gameObject;
-	
-		if (thingLeft.GetComponent<Ball>()){
-			ballEnteredBox = true;
-			standingDisplay.color = Color.black;
-		}
-		
+
 		if(thingLeft.GetComponent<Pin>()){
 			Destroy(thingLeft);
 		}
