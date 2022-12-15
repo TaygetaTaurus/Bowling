2b78c1e2802a915a922c48fca2ccfe69f56fc74e
Mikhail <fentoru@gmail.com>
2022-12-16 03:49:33 +0500

    v2.25 added score panel and back script for it

--- Assets/Scripts/PinSetter.cs
+++ Assets/Scripts/PinSetter.cs
@@ -4,37 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PinSetter : MonoBehaviour {
-	public Text standingDisplay;
 	public GameObject pinSet;
 	
-	private int lastStandingCount = -1;
-	private int lastSettledCount = 10;
-	private bool ballOutOfPlay = false;
-	private float lastChangeTime;
-	
-	private Ball ball;
 	private Animator animator;
-	
-	private ActionMaster actionMaster = new ActionMaster();
+	private PinCounter pinCounter;
 	
 	// Use this for initialization
 	void Start () {
-		ball = GameObject.FindObjectOfType<Ball>();
 		animator = GetComponent<Animator>();
+		pinCounter = GameObject.FindObjectOfType<PinCounter>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		standingDisplay.text = CountStanding().ToString();
 		
-		if (ballOutOfPlay){
-			UpdateStandingCountAndSettle();
-			standingDisplay.color = Color.red;
-		}
-	}
-	
-	public void SetBallOutOfPlay(){
-		ballOutOfPlay = true;
 	}
 	
 	public void RaisePins(){
@@ -52,63 +35,26 @@ public class PinSetter : MonoBehaviour {
 	public void Renew(){
 		Instantiate(pinSet, new Vector3(0f, 0f, 1829f), Quaternion.identity);
 	}
-	
-	public void UpdateStandingCountAndSettle(){
-		int currentStanding = CountStanding();
 		
-		if (currentStanding != lastStandingCount){
-			lastChangeTime = Time.time;
-			lastStandingCount = currentStanding;
-			return;
-		}
-		
-		float settleTime = 3f;
-		if((Time.time - lastChangeTime) > settleTime){
-			PinsHaveSettled();
-		}
-	}
-	
-	public void PinsHaveSettled(){
-		int standing = CountStanding();
-		int pinFall = lastSettledCount - standing;
-		lastSettledCount = standing;
-		
-		ActionMaster.Action action = actionMaster.Bowl(pinFall);
-	
-		if(action == ActionMaster.Action.Tidy){
-			animator.SetTrigger("tidyTrigger");
-		}else if (action == ActionMaster.Action.EndTurn){
-			animator.SetTrigger("resetTrigger");
-			lastSettledCount = 10;
-		}else if (action == ActionMaster.Action.Reset){
-			animator.SetTrigger("resetTrigger");
-			lastSettledCount = 10;
-		}else if (action == ActionMaster.Action.EndGame){
-			throw new UnityException("Dont know how to end game xd");
-		}
-		
-		ball.Reset();
-		lastStandingCount = -1;
-		ballOutOfPlay = false;
-		standingDisplay.color = Color.green;
-	}
-
-	public int CountStanding(){
-		int standing = 0;
-		
-		foreach (Pin pin in GameObject.FindObjectsOfType<Pin>()){
-			if (pin.IsStanding()){
-				standing++;
-			}
-		}
-		return standing;
-	}
-	
 	public void OnTriggerExit(Collider collider){
 		GameObject thingLeft = collider.gameObject;
 
 		if(thingLeft.GetComponent<Pin>()){
 			Destroy(thingLeft);
+		}
+	}
+	
+	public void PerformAction (ActionMaster.Action action){
+		if(action == ActionMaster.Action.Tidy){
+			animator.SetTrigger("tidyTrigger");
+		}else if (action == ActionMaster.Action.EndTurn){
+			animator.SetTrigger("resetTrigger");
+			pinCounter.Reset();
+		}else if (action == ActionMaster.Action.Reset){
+			animator.SetTrigger("resetTrigger");
+			pinCounter.Reset();
+		}else if (action == ActionMaster.Action.EndGame){
+			throw new UnityException("Dont know how to end game xd");
 		}
 	}
 }
